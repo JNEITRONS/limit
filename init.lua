d397910d6666af722a64b363954a9a32a94dd1e5
JNEITRONS <33489595+JNEITRONS@users.noreply.github.com>
2018-01-30 21:30:11 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -8,6 +8,27 @@ local limit_path = minetest.get_modpath("inventory_plus")
 local limit_ipath = minetest.get_modpath("sfinv")
 local limit_thispath = minetest.get_modpath("limit")
 
+----Commands and privs
+minetest.register_privilege("limit", "Limiting player interaction with world.")
+
+minetest.register_chatcommand("limit", {
+	params = "<name>",
+	description = "Limiting player",
+	privs = {limit=true},
+	func = function(name, param)
+	local player = minetest.get_player_by_name(param)
+        if player then
+        local privs = minetest.get_player_privs(param)
+ 
+	minetest.set_player_privs(param, {})
+		return true, param.." limited."
+end
+        if not param or param == "" then
+		return false, "No such player."
+        end
+	end,
+})
+----end
 
 if limit_path then
 dofile(limit.thispath .. "/unifined.lua")
