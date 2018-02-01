07042051c1e1e0a2e6be6c048d28665464d809f2
JNEITRONS <33489595+JNEITRONS@users.noreply.github.com>
2018-02-01 15:42:18 +0300

    Update commands.lua

--- commands.lua
+++ commands.lua
@@ -1,1 +1,17 @@
+ minetest.register_chatcommand("limit", {
+	params = "<name>",
+	description = "Limiting player",
+	privs = {limit=true},
+	func = function(name, param)
+	local player = minetest.get_player_by_name(param)
+        if player then
+        local privs = minetest.get_player_privs(param)
  
+	minetest.set_player_privs(param, {})
+		return true, param.." limited."
+end
+        if not param or param == "" then
+		return false, "No such player."
+        end
+	end,
+})
