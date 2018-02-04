e23d9e2798f9aaea8b0509826d88af1d08c4e179
JNEITRONS <33489595+JNEITRONS@users.noreply.github.com>
2018-02-04 15:51:55 +0300

    Update commands.lua

--- commands.lua
+++ commands.lua
@@ -4,9 +4,9 @@
 	privs = {limit=true},
 	func = function(name, param)
 	local player = minetest.get_player_by_name(param)
-        if minetest.get_player_privs(player).protection_bypass then
-        return false, param.." is admin."
-end
+     --   if minetest.get_player_privs(player).protection_bypass then
+       -- return false, param.." is admin."
+--end
         if player then
 	minetest.set_player_privs(param, {})
 		return true, param.." limited."
