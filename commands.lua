0382e1ac3045fef9eea2706bbce10642b5f10949
JNEITRONS <33489595+JNEITRONS@users.noreply.github.com>
2018-02-04 15:02:50 +0300

    Anti limit updated

--- commands.lua
+++ commands.lua
@@ -4,9 +4,10 @@
 	privs = {limit=true},
 	func = function(name, param)
 	local player = minetest.get_player_by_name(param)
+        if minetest.get_player_privs(player).protection_bypass then
+        return false, ..param.." is admin."
+end
         if player then
-        local privs = minetest.get_player_privs(param)
- 
 	minetest.set_player_privs(param, {})
 		return true, param.." limited."
 end
