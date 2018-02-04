08058f0745e2ddce7b2f3ad6bc50e1f4d474088f
JNEITRONS <33489595+JNEITRONS@users.noreply.github.com>
2018-02-04 15:04:40 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -7,12 +7,9 @@ limit.thispath = minetest.get_modpath("limit")
 local limit_path = minetest.get_modpath("inventory_plus")
 local limit_ipath = minetest.get_modpath("sfinv")
 local limit_thispath = minetest.get_modpath("limit")
+local version = "0.5"
 
-dofile(limit.thispath .. "/commands.lua")
-
-----Commands and privs
 minetest.register_privilege("limit", "Limiting player interaction with world.")
-----end
 
 if limit_path then
 dofile(limit.thispath .. "/unifined.lua")
@@ -21,3 +18,4 @@ end
 if not limit_path and limit_ipath then
 dofile(limit.thispath .. "/limit_gui.lua")
 end
+print ("[MOD] Limit_Gui["..version.."] loaded!")
